7cd98fb979b7abcf6e0bd33e90f0ce99f38101df
Sahan Edirimanna <edirimannasahan@gmail.com>
2025-01-31 12:23:47 +0530

    sec

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final String rosBridgeUrl = "ws://10.10.18.70:9090"; // Change to ROS PC IP
+  final String rosBridgeUrl = "ws://10.10.18.70:9090"; // Change to your ROS PC's IP
   late WebSocketChannel channel;
   bool isConnected = false;
   String rosMessage = "Test your codes here";
@@ -27,6 +27,16 @@ class _MyAppState extends State<MyApp> {
 
   void connectToRos() {
     channel = WebSocketChannel.connect(Uri.parse(rosBridgeUrl));
+
+    // Subscribe to /chatter topic
+    var subscribeMsg = {
+      "op": "subscribe",
+      "topic": "/m1n6s200_driver/out/joint_state"
+      //"topic": "/m1n6s200_driver/out/joint_angles"
+    };
+    channel.sink.add(jsonEncode(subscribeMsg));
+
+    // Listen for messages from ROS
     channel.stream.listen((message) {
       setState(() {
         rosMessage = "Received: $message";
@@ -49,7 +59,7 @@ class _MyAppState extends State<MyApp> {
       var msg = {
         "op": "publish",
         "topic": "/chatter",
-        "msg": {"data": "test 1 helloo from mac"}       
+        "msg": {"data": "test 1 hello from mac"}       
       };
       channel.sink.add(jsonEncode(msg));
     }
@@ -60,11 +70,12 @@ class _MyAppState extends State<MyApp> {
       var msg = {
         "op": "publish",
         "topic": "/chatter",
-        "msg": {"data": "test 2 helloo from mac"}       
+        "msg": {"data": "test 2 hello from mac"}       
       };
       channel.sink.add(jsonEncode(msg));
     }
   }
+
   void getInfo() {
     if (isConnected) {
       var msg = {
@@ -86,23 +97,23 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Flutter ROS ")),
+        appBar: AppBar(title: Text("Flutter ROS WebSocket")),
         body: Center(
           child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(rosMessage, style: TextStyle(fontSize: 18)),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: test1,
-            child: Text("test1"),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: test2,
-            child: Text("test2"),
-          ),
-        ],
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(rosMessage, style: TextStyle(fontSize: 18)),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: test1,
+                child: Text("Publish Test 1"),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: test2,
+                child: Text("Publish Test 2"),
+              ),
+            ],
           ),
         ),
       ),
